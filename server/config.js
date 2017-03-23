93a7fe014e87e8e4fa8e665c5d7d6df36a237d37
Erik Thompson <erik.james.thompson@gmail.com>
2017-03-23 11:05:06 -0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   API_KEY_TRANSLATE: 'AIzaSyBEb2nG4J6FMbY-3cmXBWL9nCGWp-fsx78',
-  API_KEY_VR: 'f45de3b0f3b6dda19ddc38820298804b9a313fb2'
+  API_KEY_VR: '5cd05d983e3cce45859517d313bf0a3c8e770752'
 }
